22285f516b39570dfc9972521e99d30ff0f9394b
hack4lx <38162851+attakercyebr@users.noreply.github.com>
2021-05-17 16:39:32 +0000

    Add files via upload

---
+++ install.py
@@ -0,0 +1,15 @@
+import os, sys
+
+print ("")
+print ("██╗  ██╗ █████╗  ██████╗██╗  ██╗██╗  ██╗██╗     ██╗  ██╗")
+print ("██║  ██║██╔══██╗██╔════╝██║ ██╔╝██║  ██║██║     ╚██╗██╔╝")
+print ("███████║███████║██║     █████╔╝ ███████║██║      ╚███╔╝ ")
+print ("██╔══██║██╔══██║██║     ██╔═██╗ ╚════██║██║      ██╔██╗ ")
+print ("██║  ██║██║  ██║╚██████╗██║  ██╗     ██║███████╗██╔╝ ██╗")
+print ("╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝")
+print ("")
+
+print('Pakages Installing Started')
+os.system('pip install google')
+os.system("cls" if os.name=='nt' else 'clear')
+print ('All Pakages Installed Successfully')
